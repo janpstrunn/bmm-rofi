9e9110c19a83e9a218192303a47d8e00140fc661
Janpstrunn <janpstrunn@disroot.org>
2025-07-01 17:11:46 -0300

    chore: Change homepage to github

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
         '';
         meta = {
           description = "bmm-rofi";
-          homepage = "https://codeberg.org/janpstrunn/bmm-rofi";
+          homepage = "https://github.com/janpstrunn/bmm-rofi";
           license = pkgs.lib.licenses.mit;
         };
       };
