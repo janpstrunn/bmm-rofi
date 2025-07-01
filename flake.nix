be7cbc56a5c4b3cb4a7b6bb15ab5940cf95efee3
Janpstrunn <janpstrunn@disroot.org>
2025-07-01 16:48:44 -0300

    feat: Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "A flake to run bmm-rofi with Python dependencies";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      pythonWithPackages = pkgs.python3.withPackages (ps: [
+        ps.requests
+        ps.beautifulsoup4
+      ]);
+      bmm-rofi = pkgs.stdenv.mkDerivation {
+        name = "bmm-rofi";
+        src = ./.;
+        nativeBuildInputs = [pkgs.makeWrapper pythonWithPackages];
+        installPhase = ''
+          mkdir -p $out/bin
+          cp bmm-rofi $out/bin/bmm-rofi
+          chmod +x $out/bin/bmm-rofi
+          wrapProgram "$out/bin/bmm-rofi" \
+            --prefix PATH : "${pkgs.lib.makeBinPath [pythonWithPackages]}"
+        '';
+        meta = {
+          description = "bmm-rofi";
+          homepage = "https://codeberg.org/janpstrunn/bmm-rofi";
+          license = pkgs.lib.licenses.mit;
+        };
+      };
+    in {
+      packages.default = bmm-rofi;
+      apps.default = {
+        type = "app";
+        program = "${bmm-rofi}/bin/bmm-rofi";
+      };
+    });
+}
